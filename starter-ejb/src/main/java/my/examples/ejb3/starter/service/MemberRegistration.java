c14993923844d6e9460ca2eecbf15116a0327851
Armin Vogt <avogt@s-und-n.de>
2012-09-21 13:13:49 +0200

    2

--- starter-ejb/src/main/java/my/examples/ejb3/starter/service/MemberRegistration.java
+++ starter-ejb/src/main/java/my/examples/ejb3/starter/service/MemberRegistration.java
@@ -8,7 +8,7 @@ import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import java.util.logging.Logger;
 
-// The @Stateless annotation eliminates the need for manual transaction demarcation
+// The @Stateless annotation eliminates the need for manual transaction demarcation 
 @Stateless
 public class MemberRegistration {
 
